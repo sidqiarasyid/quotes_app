5572599489b5cbb3d70caf1d34f7bbedb280d031
sidqi <sidqiarasyid31@gmail.com>
2022-07-18 17:51:41 +0700

    feat: [SA] list tambahan

---
+++ lib/Model/modelTambahData.dart
@@ -0,0 +1,31 @@
+class ModelTambahData {
+  String item;
+  String tebal;
+  String catatan;
+
+  ModelTambahData(this.item, this.tebal, this.catatan);
+
+  String get getItem {
+    return item;
+  }
+
+  String get getTebal {
+    return tebal;
+  }
+
+  String get getCatatan {
+    return catatan;
+  }
+
+  set setItem(String name) {
+    this.item = item;
+  }
+
+  set setTebal(String time) {
+    this.tebal = tebal;
+  }
+
+  set setCatatan(String desc) {
+    this.catatan = catatan;
+  }
+}
